71aeab4f40fba66b3f73533b2cae2b060ad7198b
Fabian Schuh <mail@xeroc.org>
2025-10-08 15:52:01 +0200

    ♻️  refactor: get rid of some account details stuff in the app

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -6,25 +6,15 @@ import { lazy } from 'react'
 const links = [
   //
   { label: 'Home', path: '/' },
-  { label: 'Account', path: '/account' },
   { label: 'Create Gateway', path: '/gateway' },
   { label: 'Create Policy', path: '/policy' },
 ]
 const LazyDashboard = lazy(() => import('@/components/dashboard/dashboard-feature'))
-const LazyAccountIndex = lazy(() => import('@/components/account/account-index-feature'))
-const LazyAccountDetail = lazy(() => import('@/components/account/account-detail-feature'))
 const LazyGateway = lazy(() => import('@/components/gateway/gateway-feature'))
 const LazyPaymentPolicy = lazy(() => import('@/components/payment-policy/payment-policy-feature'))
 
 const routes: RouteObject[] = [
   { index: true, element: <LazyDashboard /> },
-  {
-    path: 'account',
-    children: [
-      { index: true, element: <LazyAccountIndex /> },
-      { path: ':address', element: <LazyAccountDetail /> },
-    ],
-  },
   { path: 'gateway', element: <LazyGateway /> },
   { path: 'policy', element: <LazyPaymentPolicy /> },
 ]
