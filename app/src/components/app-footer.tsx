f310ae5287b9d7dd135f5d720900d2305150e805
Fabian Schuh <mail@xeroc.org>
2025-10-26 19:44:06 +0100

    🐛 fix: socials and footer

--- app/src/components/app-footer.tsx
+++ app/src/components/app-footer.tsx
@@ -2,13 +2,7 @@ import { BorderedContainer } from '@/components/ui/bordered-container'
 
 export function AppFooter() {
   return (
-    <div
-      style={{
-        fontFamily: 'var(--font-primary)',
-        marginTop: 'auto',
-        marginBottom: '21px',
-      }}
-    >
+    <div className="pb-[3px] sm:pb-[23px]">
       <BorderedContainer
         borderSides={['right', 'bottom', 'left']}
         className="flex flex-col md:flex-row items-center justify-center md:justify-between"
@@ -20,44 +14,54 @@ export function AppFooter() {
         }}
       >
         {/* Copyright */}
-        <div className="text-center md:text-left" style={{ textTransform: 'uppercase' }}>
-          ( 2025 TRIBUTARY ALL RIGHTS RESERVED )
-        </div>
+        <div className="text-center md:text-left uppercase">(2025 TRIBUTARY ALL RIGHTS RESERVED)</div>
 
         {/* Legal Section */}
-        <div className="flex items-center justify-center gap-2 mt-2 md:mt-0">
-          <div style={{ textTransform: 'uppercase' }}>( LEGAL</div>
-          <a href="#" className="relative cursor-pointer group" style={{ textTransform: 'uppercase' }}>
-            TERMS OF USE
-            <span className="absolute bottom-[-2px] left-0 h-[1px] w-0 bg-[var(--color-primary)] transition-all duration-300 group-hover:w-full" />
-          </a>
-          <div>/</div>
-          <a href="#" className="relative cursor-pointer group" style={{ textTransform: 'uppercase' }}>
-            PRIVACY POLICY
-            <span className="absolute bottom-[-2px] left-0 h-[1px] w-0 bg-[var(--color-primary)] transition-all duration-300 group-hover:w-full" />
-          </a>
-          <div style={{ textTransform: 'uppercase' }}>)</div>
-        </div>
+        {/* <div className="flex items-center justify-center gap-2 mt-2 md:mt-0"> */}
+        {/*   <div style={{ textTransform: 'uppercase' }}>( LEGAL</div> */}
+        {/*   <a href="#" className="relative cursor-pointer group uppercase"> */}
+        {/*     TERMS OF USE */}
+        {/*     <span className="absolute bottom-[-2px] left-0 h-[1px] w-0 bg-[var(--color-primary)] transition-all duration-300 group-hover:w-full" /> */}
+        {/*   </a> */}
+        {/*   <div>/</div> */}
+        {/*   <a href="#" className="relative cursor-pointer group uppercase"> */}
+        {/*     PRIVACY POLICY */}
+        {/*     <span className="absolute bottom-[-2px] left-0 h-[1px] w-0 bg-[var(--color-primary)] transition-all duration-300 group-hover:w-full" /> */}
+        {/*   </a> */}
+        {/*   <div style={{ textTransform: 'uppercase' }}>)</div> */}
+        {/* </div> */}
 
         {/* Social Section */}
         <div className="flex items-center justify-center gap-2 mt-2 md:mt-0">
-          <div style={{ textTransform: 'uppercase' }}>( SOCIAL</div>
+          <div>(</div>
           <a
-            href="https://twitter.com"
+            href="https://x.com/tributaryso"
             target="_blank"
             rel="noopener noreferrer"
-            className="relative cursor-pointer group"
-            style={{ textTransform: 'uppercase' }}
+            className="relative cursor-pointer group uppercase"
           >
             X
             <span className="absolute bottom-[-2px] left-0 h-[1px] w-0 bg-[var(--color-primary)] transition-all duration-300 group-hover:w-full" />
           </a>
           <div>/</div>
-          <a href="#" className="relative cursor-pointer group" style={{ textTransform: 'uppercase' }}>
+          <a
+            href="https://t.me/tributary_so"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="relative cursor-pointer group uppercase"
+          >
+            Telegram
+            <span className="absolute bottom-[-2px] left-0 h-[1px] w-0 bg-[var(--color-primary)] transition-all duration-300 group-hover:w-full" />
+          </a>
+          <div>/</div>
+          <a
+            href="https://arena.colosseum.org/projects/explore/tributary-1"
+            className="relative cursor-pointer group uppercase"
+          >
             COLOSSEUM
             <span className="absolute bottom-[-2px] left-0 h-[1px] w-0 bg-[var(--color-primary)] transition-all duration-300 group-hover:w-full" />
           </a>
-          <div style={{ textTransform: 'uppercase' }}>)</div>
+          <div>)</div>
         </div>
       </BorderedContainer>
     </div>
