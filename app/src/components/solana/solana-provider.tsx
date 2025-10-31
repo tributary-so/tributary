39a1f33bd263bcf4150a5e8f48de84c174686600
Fabian Schuh <mail@xeroc.org>
2025-10-31 19:28:51 +0100

    🐛 fix: downgrade wallet-adapter, remove unecessary inputs

--- app/src/components/solana/solana-provider.tsx
+++ app/src/components/solana/solana-provider.tsx
@@ -1,8 +1,6 @@
 import { WalletError } from '@solana/wallet-adapter-base'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui'
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom'
-import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare'
 import React, { useCallback, useMemo } from 'react'
 import { useCluster } from '../cluster/cluster-data-access'
 import '@solana/wallet-adapter-react-ui/styles.css'
@@ -12,14 +10,13 @@ export { WalletMultiButton as WalletButton }
 export function SolanaProvider({ children }: { children: React.ReactNode }) {
   const { cluster } = useCluster()
   const endpoint = useMemo(() => cluster.endpoint, [cluster])
-  const wallets = useMemo(() => [new PhantomWalletAdapter(), new SolflareWalletAdapter()], [])
   const onError = useCallback((error: WalletError) => {
     console.error(error)
   }, [])
 
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} onError={onError} autoConnect={true}>
+      <WalletProvider wallets={[]} onError={onError} autoConnect={true}>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
