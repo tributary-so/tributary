3e41f88013f4e81a22e422a3744f524d5cf655dc
Fabian Schuh <mail@xeroc.org>
2026-02-03 11:41:49 +0000

    ♻️ refactor: intermediate step

--- apps/lando/vite.config.ts
+++ apps/lando/vite.config.ts
@@ -1,7 +1,49 @@
+import tailwindcss from "@tailwindcss/vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import inject from "@rollup/plugin-inject";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      globals: {
+        Buffer: process.env.NODE_ENV == "development",
+      },
+      include: ["buffer"],
+    }),
+    tailwindcss(),
+  ],
+  server: {
+    host: "0.0.0.0",
+  },
+  build: {
+    target: "esnext", // Output ESNext code
+    rollupOptions: {
+      plugins: [inject({ Buffer: ["buffer", "Buffer"] })],
+      external: [
+        "vite-plugin-node-polyfills/shims/buffer",
+        "stream",
+        "http",
+        "https",
+        "zlib",
+      ],
+      output: {
+        manualChunks: {
+          "ui-vendor": ["@tailwindcss/vite"],
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ["stream"], // Ensure stream is bundled during development
+    esbuildOptions: {
+      target: "esnext", // Ensure dependency pre-bundling supports ESNext
+      supported: {
+        "import-assertions": true, // Explicitly enable import assertions in esbuild
+      },
+    },
+  },
 });
