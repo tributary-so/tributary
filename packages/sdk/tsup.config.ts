6c553107640b8caabd60ff272160453c55321dc7
Fabian Schuh <mail@xeroc.org>
2026-02-25 18:00:30 +0100

    👷 build(sdk): use tsup for building

---
+++ packages/sdk/tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "tsup";
+import { fixImportsPlugin } from "esbuild-fix-imports-plugin";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  target: "es2020",
+  outDir: "dist/packages/sdk/src",
+  dts: true,
+  clean: true,
+  sourcemap: true,
+  esbuildPlugins: [fixImportsPlugin()],
+  platform: "node",
+  external: [],
+});
