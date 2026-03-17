55aa3562dd778634b5848cf1076b3ac195780868
Fabian Schuh <mail@xeroc.org>
2026-03-17 13:13:19 +0100

    ✨ feat: futardio raise
    ✨ feat: make banner fit into rebranded design

--- apps/landing/src/App.tsx
+++ apps/landing/src/App.tsx
@@ -2,13 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import Home from "./pages/Home";
+import FutardioBanner from "./components/futardio-banner";
 
 export default function App() {
   return (
     <div className="min-h-screen bg-background antialiased font-sans">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={
+          <>
+            <FutardioBanner />
+            <Home />
+          </>} />
       </Routes>
       <Footer />
     </div>
