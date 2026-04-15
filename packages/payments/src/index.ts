415fddedfe4773f07a580f5433129c82f7874fb4
Fabian Schuh <mail@xeroc.org>
2026-04-15 21:14:15 +0200

    feat: proper payments verifications via payments package

--- packages/payments/src/index.ts
+++ packages/payments/src/index.ts
@@ -1,11 +1,9 @@
-// Main exports for @tributary-so/payments package
 export * from "./core/client";
 export * from "./core/session";
 export * from "./core/tracking";
 export * from "./core/onetime";
+export * from "./core/verification";
 
-// Type exports
 export * from "./types/tributary";
 
-// Utility exports
 export * from "./utils/validation";
