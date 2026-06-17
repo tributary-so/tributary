6953b5274b178b2241d0cb4e7cbd3ca6bb48ce7c
Fabian Schuh <mail@xeroc.org>
2026-06-17 10:14:39 +0200

    ✨ feat: allow to inspect individual pda content in cli

--- apps/cli/src/commands/pda/gateway.ts
+++ apps/cli/src/commands/pda/gateway.ts
@@ -1,12 +1,12 @@
-import {Flags} from '@oclif/core'
+import { Flags } from '@oclif/core'
 
-import {ReadOnlyCommand} from '../../lib/base-command.js'
-import {parsePublicKey} from '../../lib/utils.js'
+import { ReadOnlyCommand } from '../../lib/base-command.js'
+import { parsePublicKey } from '../../lib/utils.js'
 
 export default class PdaGateway extends ReadOnlyCommand {
   static description = 'Get gateway PDA address'
-static examples = ['<%= config.bin %> <%= command.id %> --authority GATEWAY_AUTHORITY_PUBKEY']
-static flags = {
+  static examples = ['<%= config.bin %> <%= command.id %> --authority GATEWAY_AUTHORITY_PUBKEY']
+  static flags = {
     ...ReadOnlyCommand.baseFlags,
     authority: Flags.string({
       char: 'a',
@@ -16,12 +16,13 @@ static flags = {
   }
 
   public async run(): Promise<void> {
-    const {flags} = await this.parse(PdaGateway)
+    const { flags } = await this.parse(PdaGateway)
     const authority = parsePublicKey(flags.authority)
     if (!authority) throw new Error('Invalid authority')
 
     const sdk = await this.getSDK()
     const pda = sdk.getGatewayPda(authority)
+    const pdaData = sdk.getPaymentGateway(pda.address)
 
     this.output({
       command: 'pda gateway',
@@ -29,6 +30,7 @@ static flags = {
         address: pda.address.toString(),
         authority: authority.toString(),
         bump: pda.bump,
+        data: pdaData,
         type: 'gateway',
       },
       success: true,
